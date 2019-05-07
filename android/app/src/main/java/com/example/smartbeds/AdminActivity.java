121ecb442cf20a903214fc427f6ef0e48ca5377a
aog0036 <aog0036@alu.ubu.es>
2019-05-07 15:29:49 +0200

    Probada la recepción de las camas disponibles

--- android/app/src/main/java/com/example/smartbeds/AdminActivity.java
+++ android/app/src/main/java/com/example/smartbeds/AdminActivity.java
@@ -1,13 +1,29 @@
 package com.example.smartbeds;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class AdminActivity extends AppCompatActivity {
+
+    private final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin);
+
+        Session session = Session.getInstance();
+        if(session.getToken()==null) {
+            Intent intent = new Intent(context, MainActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    public void visualizarCamas(View view){
+        Intent intent = new Intent(context, BedsActivity.class);
+        startActivity(intent);
     }
 }
--- android/app/src/main/java/com/example/smartbeds/BedsActivity.java
+++ android/app/src/main/java/com/example/smartbeds/BedsActivity.java
@@ -1,13 +1,70 @@
 package com.example.smartbeds;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class BedsActivity extends AppCompatActivity {
+
+    private final Context context = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beds);
+
+        Session session = Session.getInstance();
+        if(session.getToken()==null) {
+            Intent intent = new Intent(context, MainActivity.class);
+            startActivity(intent);
+        }
+
+        String urlParameters = "token="+session.getToken();
+        Communication communication = new Communication("/api/beds", urlParameters);
+        Thread thread = new Thread(communication);
+
+        try {
+            thread.start();
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        int status = communication.getStatus();
+        Log.d("STATUS", ""+status);
+
+        JSONObject resultado = communication.getResult();
+        JSONArray bedNames=null;
+        try {
+            bedNames = (JSONArray) resultado.get("beds");
+
+            Log.d("BEDS", bedNames.toString());
+
+            for(int i=0; i< bedNames.length(); i++){
+                String bedName = (String) bedNames.get(i);
+                urlParameters = "token="+session.getToken()+"&bedname="+bedName;
+                communication = new Communication("/api/bed", urlParameters);
+                thread = new Thread(communication);
+
+                try {
+                    thread.start();
+                    thread.join();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 }
