c112004346a310124ad28807226514e52e5dff2c
aog0036 <aog0036@alu.ubu.es>
2019-06-26 11:52:28 +0200

    Añadir referencias a las herramientas de test

--- android/app/src/main/java/com/example/smartbeds/generalActivities/AboutActivity.java
+++ android/app/src/main/java/com/example/smartbeds/generalActivities/AboutActivity.java
@@ -29,8 +29,8 @@ public class AboutActivity extends AppCompatActivity {
         TextView text4 = findViewById(R.id.about_text4);
         text1.setText(Html.fromHtml("<b>"+"SmartBeds"+"</b>"+" es un detector automático de crisis epilépticas que emplea datos tomados mediante una matriz de sensores de presión instalados en un colchón. Esta aplicación Android permite gestionar los usuarios y las camas registradas y monitorizar el estado de los pacientes en tiempo real."));
         text2.setText(Html.fromHtml("Este clasificador ha sido desarrollado por "+"<b>"+"José Luis Garrido Labrador"+"</b>"+" y "+"<b>"+"Alicia Olivares Gil"+"</b>"+" como parte del proyecto MINERÍA DE DATOS APLICADA A LA DETECCIÓN DE CRISIS EPILÉPTICAS tutorizado por el "+"<b>"+"Dr. Álvar Arnaiz González"+"</b>"+" y el "+"<b>"+"Dr. José Francios Díez Pastor"+"</b>"+" del grupo ADMIRABLE del Área de Lenguajes y Sistemas Informáticos del departamento de Ingeniería Civil de la Universidad de Burgos."));
-        text3.setText("Este proyecto está alojado en la plataforma GitHub bjo la licencia AGPLv3: https://github.com/aog0036/TFG-SmartBeds");
-        text4.setText(Html.fromHtml("- Android Support Library: Apache License 2.0"+"<br>"+"- MPAndroidChart: Apache License 2.0"+"<br>"+"- Socket.IO-client Library: MIT License"));
+        text3.setText("Este proyecto está alojado en la plataforma GitHub bajo la licencia AGPLv3: https://github.com/aog0036/TFG-SmartBeds");
+        text4.setText(Html.fromHtml("- Android Support Library: Apache License 2.0"+"<br>"+"- MPAndroidChart: Apache License 2.0"+"<br>"+"- Gradle: Apache License 2.0"+"<br>"+"- Socket.IO-client Library: MIT License"));
     }
 
     /**
